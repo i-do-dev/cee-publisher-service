5ebb10a3dcc7e49fdf449b59946fe62de30582cc
Waqar Muneer <i.do.dev@outlook.com>
2024-02-06 12:54:23 +0500

    update seeders and API key field to text :hammer:

--- seeders/20240205072237-insert-client-roles.js
+++ seeders/20240205072237-insert-client-roles.js
@@ -3,15 +3,27 @@ const { v4: uuidv4 } = require('uuid');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('ClientRole', [{
-      id: uuidv4(),
-      name: 'cee-publisher-tool',
-      description: 'Authorizes the C2E Publisher Tool to access the relevant resources'
-    }, {
-      id: uuidv4(),
-      name: 'cee-store',
-      description: 'Authorizes the C2E Player to access the relevant resources'
-    }], {});
+
+    // query ClientRole where name = 'cee-publisher-tool' and 'cee-store'
+    // insert into ClientRole if not exists
+    const ceePublisherToolRole = await queryInterface.sequelize.query(
+      `SELECT id FROM "ClientRole" WHERE name = 'cee-publisher-tool';`
+    );
+    const ceeStoreRole = await queryInterface.sequelize.query(
+      `SELECT id FROM "ClientRole" WHERE name = 'cee-store';`
+    );
+    const ok = ceePublisherToolRole[0][0] && ceeStoreRole[0][0];
+    if (!ok) {
+      return await queryInterface.bulkInsert('ClientRole', [{
+        id: uuidv4(),
+        name: 'cee-publisher-tool',
+        description: 'Authorizes the C2E Publisher Tool to access the relevant resources'
+      }, {
+        id: uuidv4(),
+        name: 'cee-store',
+        description: 'Authorizes the C2E Player to access the relevant resources'
+      }], {});
+    }
   },
 
   down: async (queryInterface, Sequelize) => {
--- seeders/20240205173130-c2e-publisher-service.js
+++ seeders/20240205173130-c2e-publisher-service.js
@@ -3,13 +3,20 @@ const { v4: uuidv4 } = require('uuid');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('Service', [{
-      id: uuidv4(),
-      name: 'C2E Publisher Service',
-      description: 'C2E Publisher Service reference implementation',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }], {});
+    // query Service count to check if thers no existing Service
+    const serviceCount = await queryInterface.sequelize.query(
+      `SELECT COUNT(*) FROM "Service";`
+    );
+    const ok = serviceCount[0][0].count == 0;
+    if (ok) {
+      return await queryInterface.bulkInsert('Service', [{
+        id: uuidv4(),
+        name: 'C2E Publisher Service',
+        description: 'C2E Publisher Service reference implementation',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }], {});
+    }
   },
   down: async (queryInterface, Sequelize) => {
     await queryInterface.bulkDelete('Service', null, {});
